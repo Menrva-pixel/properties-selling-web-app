36a7bad4ab471f7ec1e7694be7b40f7a0e017c43
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-11-06 18:42:16 +0700

    membuat logic login dan register

--- auth/login.php
+++ auth/login.php
@@ -1,3 +1,40 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['log'])) {
+    header("location: ../index.php");
+} else {
+}
+;
+
+include '../include/database.php';
+date_default_timezone_set('Asia/Jakarta');
+$timenow = date("Y-m-d H:i:s");
+
+if (isset($_POST['submit'])) {
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $queryuser = mysqli_query($conn, "SELECT * FROM login WHERE email = '$email'");
+    $cariuser = mysqli_fetch_assoc($queryuser);
+
+    if (password_verify($password, $cariuser['password'])) {
+        $_SESSION['id'] = $cariuser['id'];
+        $_SESSION['role'] = $cariuser['role'];
+        $_SESSION['notelp'] = $cariuser['notelp'];
+        $_SESSION['name'] = $cariuser['namalengkap'];
+        $_SESSION['log'] = "Logged";
+        header("location: ../index.php");
+    } else {
+        echo "
+        <script>
+        alert('Username atau Password Anda Salah');
+        document.location.href = 'login.php';
+        </script>
+        <meta http-equiv='refresh' content='0; url=login.php'>";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,16 +46,16 @@
 <body class="bg-gray-200 h-screen flex items-center justify-center">
     <div class="bg-white p-8 rounded shadow-md max-w-xs">
         <h2 class="text-2xl mb-4">Login</h2>
-        <form action="login_act.php" method="post">
+        <form method="post">
             <div class="mb-4">
-                <label for="username" class="block text-gray-600">Username</label>
-                <input type="text" id="username" name="username" class="w-full px-3 py-2 border rounded">
+                <label for="email" class="block text-gray-600">Email</label>
+                <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded">
             </div>
             <div class="mb-4">
                 <label for="password" class="block text-gray-600">Password</label>
                 <input type="password" id="password" name="password" class="w-full px-3 py-2 border rounded">
             </div>
-            <button type="submit" class="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">Login</button>
+            <button type="submit" name="submit" class="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">Login</button>
         </form>
         <div class="mt-4">
             <p>Belum punya akun? <a href="register.php" class="text-blue-500">Registrasi disini</a></p>
